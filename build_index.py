c2cc2f2be2515cfe66038705a262cd8f076bc236
obiettivocambiamento-ui <obiettivocambiamento@gmail.com>
2026-04-26 19:22:41 +0200

    Update build_index.py

--- build_index.py
+++ build_index.py
@@ -1,18 +1,1 @@
-from sentence_transformers import SentenceTransformer
-import json
-import faiss
-import numpy as np
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
-
-with open("chunks.json","r",encoding="utf-8") as f:
-    chunks = json.load(f)
-
-embeddings = model.encode(chunks)
-
-dimension = embeddings[0].shape[0]
-index = faiss.IndexFlatL2(dimension)
-
-index.add(np.array(embeddings))
-
-faiss.write_index(index, "site.index")
